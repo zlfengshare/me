782f15b074f26966aba54fe43ed03fb354bbac94
uni1028 <zlfengshare@gmail.com>
2022-09-27 18:32:27 +0800

    Add files via upload

---
+++ qpy.js
@@ -0,0 +1,18 @@
+/******************************
+qpy.js
+
+^https?:\/\/apis40\.qupeiyin\.com\/member\/index url script-response-body qpy.js
+
+[mitm] 
+
+hostname = apis40.qupeiyin.com
+
+******************************/
+
+var body = $response.body
+      .replace(/vip\":.*?,/g, 'vip\": "1",')
+      .replace(/endtime\":.*?,/g, 'endtime\": "1979889097",');
+      
+$done({ body });
+
+
